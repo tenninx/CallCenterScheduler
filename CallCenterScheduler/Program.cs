e28b1277b1e63daee35679627526853a8a79be2c
Hee Lup Chong <neonin@gmail.com>
2025-07-11 17:23:24 +0800

    - Added WorkingEnvironment class to storing working variables. - Added support of settings through input. - Added more prompt for input description.

--- CallCenterScheduler/Program.cs
+++ CallCenterScheduler/Program.cs
@@ -4,25 +4,21 @@ using System.Linq;
 
 class Program
 {
-    static List<Worker> Workers;
-    static List<Group> FinishedGroups;
-    static List<Group> ListOfGroups;
-    static int NumOfWorkers;
-    static int WaitedTime;
-
     public static void Main()
     {
-        // G
-        int numOfTopGroups = 2;
-        // C
-        int numOfTopCategories = 2;
-        // N
-        NumOfWorkers = 3;
-
         string? input;
         while (true)
         {
-            Console.WriteLine("Enter the input in the following format: A string of semicolon-separated data in the form \"category,group,time\" followed by zero or more prerequisites of the form \",category,group\", empty input to terminate:");
+            Console.WriteLine(@"Input format: {G},{C},{N}-{input_data} as in:
+G: Top number of groups (default is 3). Enter 0 to return only the completion time. Negative not accepted.
+C: Top number of categories (default is 3). Enter 0 to return only the completion time. Negative not accepted.
+N: Number of workers (default is 2). Enter 0 for unlimited number of workers. Negative not accepted.
+input_data: Semicolon-separated data in the form ""category,group,time"" followed by zero or more prerequisites of the form "",category,group"", e.g. Home,OR Jefferson,5444;Medicare,OR Lake,1304;Medicare,WA King,43061;Life,OR Other,12806,Medicare,OR Lake;Life,WA Other,70944,Medicare,WA King
+You need to specify all G, C and N at once or none by specifying {input_data} directly to use the default settings.
+Example input with settings: 2,2,0-Home,OR Jefferson,5444;Medicare,OR Lake,1304;Medicare,WA King,43061;Life,OR Other,12806,Medicare,OR Lake;Life,WA Other,70944,Medicare,WA King
+Example input using defaults: Home,OR Jefferson,5444;Medicare,OR Lake,1304;Medicare,WA King,43061;Life,OR Other,12806,Medicare,OR Lake;Life,WA Other,70944,Medicare,WA King
+Leave it empty and press enter to terminate. Enter your input:");
+            
             input = Console.ReadLine();
 
             if (string.IsNullOrEmpty(input)) break;
@@ -31,7 +27,7 @@ class Program
 
             if (!queryResult.IsValidInput)
             {
-                Console.WriteLine("Error Message: " + queryResult.ErrorMessage);
+                Console.WriteLine("Error Message: " + queryResult.ErrorMessage + "\n");
                 continue;
             }
 
@@ -39,19 +35,26 @@ class Program
 
             if (!queryResult.IsValidInput)
             {
-                Console.WriteLine("Error Message: " + queryResult.ErrorMessage);
+                Console.WriteLine("Error Message: " + queryResult.ErrorMessage + "\n");
                 continue;
             }
 
-            Workers = new List<Worker>();
-            FinishedGroups = new List<Group>();
+            Init();
 
             Schedule();
 
-            var result = GenerateResult(numOfTopGroups, numOfTopCategories);
+            var result = GenerateResult(WorkingEnvironment.NumOfTopGroups, WorkingEnvironment.NumOfTopCategories);
 
             Console.WriteLine("Result: " + result);
+            Console.WriteLine();
         }
+    }
+
+    static void Init()
+    {
+        WorkingEnvironment.Workers = new List<Worker>();
+        WorkingEnvironment.FinishedGroups = new List<Group>();
+        WorkingEnvironment.WaitedTime = 0;
     }
 
     /// <summary>
@@ -61,13 +64,38 @@ class Program
     /// <returns>Parsed data object</returns>
     static QueryResult ParseInput(string p_strInput)
     {
-        string[] strGroups = p_strInput.Split(';');
+        p_strInput = p_strInput.Replace("\\,", ",").Replace("\\;", ";").Trim();
+
+        if (p_strInput.Contains("-"))
+        {
+            string[] settings = p_strInput.Substring(0, p_strInput.IndexOf("-")).Split(",");
+            if (settings.Length != 3)
+                return GenerateResult(false, "Invalid settings input. Expected format: G,C,N. Either specify all or none for using defaults.");
+
+            int number;
+
+            if (!int.TryParse(settings[0].Trim(), out number))
+                return GenerateResult(false, "Invalid settings input for G. Only a numeric value is accepted.");
+            WorkingEnvironment.NumOfTopGroups = number < 0 ? 0 : number;
+
+            if (!int.TryParse(settings[1].Trim(), out number))
+                return GenerateResult(false, "Invalid settings input for C. Only a numeric value is accepted.");
+            WorkingEnvironment.NumOfTopCategories = number;
+
+            if (!int.TryParse(settings[2].Trim(), out number))
+                return GenerateResult(false, "Invalid settings input for N. Only a numeric value is accepted.");
+            WorkingEnvironment.NumOfWorkers = number == 0 ? int.MaxValue : number;
+        }
+
+        p_strInput = p_strInput.Substring(p_strInput.IndexOf("-") + 1).Trim();
+
+        string[] strGroups = p_strInput.Split(";");
 
         List<Group> objGroups = new List<Group>();
 
         foreach (var strGroup in strGroups)
         {
-            string[] strGroupDetails = strGroup.Split(',');
+            string[] strGroupDetails = strGroup.Trim().Split(',');
             List<Group> objPrereqs = new List<Group>();
 
             if (strGroupDetails.Length < 3)
@@ -101,24 +129,24 @@ class Program
             objGroups.Add(objGroup);
         }
 
-        ListOfGroups = objGroups;
+        WorkingEnvironment.ListOfGroups = objGroups;
 
-        return GenerateResult(true, null, objGroups);
+        return GenerateResult(true);
     }
 
     private static QueryResult LinkPrerequisites()
     {
-        foreach (var group in ListOfGroups)
+        foreach (var group in WorkingEnvironment.ListOfGroups)
         {
             for (int i = 0; i < group.PrerequisiteGroups.Count; i++)
             {
-                var foundGroup = ListOfGroups.FirstOrDefault(g => g.Name == group.PrerequisiteGroups[i].Name && g.Category == group.PrerequisiteGroups[i].Category);
+                var foundGroup = WorkingEnvironment.ListOfGroups.FirstOrDefault(g => g.Name == group.PrerequisiteGroups[i].Name && g.Category == group.PrerequisiteGroups[i].Category);
 
                 if (foundGroup == null)
                     return GenerateResult(false, "Prerequisite '" + String.Join(",", group.PrerequisiteGroups[i].Category, group.PrerequisiteGroups[i].Name) + " is not found.");
 
                 if (foundGroup.PrerequisiteGroups.Count > 0)
-                    return GenerateResult(false, "Nested prerequisites at " + String.Join(",", group.Category, group.Name) + " are not supported.");
+                    return GenerateResult(false, "Nested prerequisites at '" + String.Join(",", group.Category, group.Name) + "' are not supported.");
 
                 group.PrerequisiteGroups[i] = foundGroup;
             }
@@ -129,7 +157,7 @@ class Program
 
     static void Schedule()
     {
-        Queue<Group> p_objQueueOfGroups = new Queue<Group>(ListOfGroups.OrderBy(l => l.PrerequisiteGroups.Count));
+        Queue<Group> p_objQueueOfGroups = new Queue<Group>(WorkingEnvironment.ListOfGroups.OrderBy(l => l.PrerequisiteGroups.Count));
 
         while (true)
         {
@@ -147,14 +175,14 @@ class Program
     {
         Worker worker;
 
-        if (Workers.Count < NumOfWorkers)
+        if (WorkingEnvironment.Workers.Count < WorkingEnvironment.NumOfWorkers)
         {
-            worker = new Worker() { ID = Workers.Count };
-            Workers.Add(worker);
+            worker = new Worker() { ID = WorkingEnvironment.Workers.Count };
+            WorkingEnvironment.Workers.Add(worker);
         }
         else
         {
-            worker = Workers.OrderBy(w => w.NextAvailableTime).Take(1).First();
+            worker = WorkingEnvironment.Workers.OrderBy(w => w.NextAvailableTime).Take(1).First();
             worker.WorkingOn = null;
         }
 
@@ -167,7 +195,7 @@ class Program
 
         if (p_objWorker.NextAvailableTime < p_objGroup.MinTimeBeforeStart)
         {
-            WaitedTime += p_objGroup.MinTimeBeforeStart - p_objWorker.NextAvailableTime;
+            WorkingEnvironment.WaitedTime += p_objGroup.MinTimeBeforeStart - p_objWorker.NextAvailableTime;
             p_objWorker.NextAvailableTime = p_objGroup.MinTimeBeforeStart;
         }
 
@@ -176,7 +204,7 @@ class Program
 
         p_objGroup.IsCompleted = true;
         p_objWorker.FinishedGroups.Add(p_objGroup);
-        FinishedGroups.Add(p_objGroup);
+        WorkingEnvironment.FinishedGroups.Add(p_objGroup);
     }
 
     private static Group? GetNextGroup(Queue<Group> p_objQueueOfGroups, Worker p_objWorker)
@@ -212,7 +240,7 @@ class Program
 
     private static QueryResult GetPrerequisiteGroups(Group p_objGroup, Worker p_objWorker)
     {
-        var finishedGroup = FinishedGroups.Find(x => x == p_objGroup.PrerequisiteGroups.First());
+        var finishedGroup = WorkingEnvironment.FinishedGroups.Find(x => x == p_objGroup.PrerequisiteGroups.First());
         if (finishedGroup != null)
             p_objGroup.MinTimeBeforeStart = finishedGroup.StartTime + finishedGroup.RequiredTime;
         else
@@ -228,7 +256,7 @@ class Program
     static string GenerateResult(int G, int C)
     {
         SortedSet<string> groupNames = new SortedSet<string>();
-        var temp = ListOfGroups.OrderByDescending(g => g.RequiredTime).ToList();
+        var temp = WorkingEnvironment.ListOfGroups.OrderByDescending(g => g.RequiredTime).ToList();
         HashSet<string> categories = new HashSet<string>();
 
         for (int i = 0; i < temp.Count; i++)
@@ -241,7 +269,7 @@ class Program
             groupNames.Add(temp[i].Name);
         }
 
-        var highestTime = Workers.OrderByDescending(w => w.NextAvailableTime).First();
+        var highestTime = WorkingEnvironment.Workers.OrderByDescending(w => w.NextAvailableTime).First();
 
         var topGroups = string.Empty;
         if (groupNames.Count > 0)
@@ -271,6 +299,20 @@ class Program
         };
     }
     #endregion
+}
+
+/// <summary>
+/// Temporary storage class for working environment data. Should be reset after each input.
+/// </summary>
+public static class WorkingEnvironment
+{
+    public static List<Worker> Workers;
+    public static List<Group> FinishedGroups;
+    public static List<Group> ListOfGroups;
+    public static int NumOfTopGroups = 3;
+    public static int NumOfTopCategories = 1;
+    public static int NumOfWorkers = 2;
+    public static int WaitedTime;
 }
 
 public class Worker
